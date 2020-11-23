89c95a26c5eee4791adb31df8fe70eb60112aa38
KSneijders <k.sneijders@st.hanze.nl>
2020-11-23 14:11:17 +0100

    Added enums

---
+++ openpylivox/enums.py
@@ -0,0 +1,21 @@
+from enum import IntEnum
+
+
+class DataType(IntEnum):
+    CARTESIAN = 0
+    SPHERICAL = 1
+
+
+class FileType(IntEnum):
+    StoredASCII = 0
+    RealtimeASCII = 1
+    RealtimeBINARY = 2
+
+
+class FirmwareType(IntEnum):
+    SINGLE_RETURN = 1
+    """Relevant for Mid-40 and Mid-100"""
+    DOUBLE_RETURN = 2
+    """Mid-40 and Mid-100 only"""
+    TRIPLE_RETURN = 3
+    """Mid-40 and Mid-100 only"""
